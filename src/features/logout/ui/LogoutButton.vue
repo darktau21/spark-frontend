73cb6212ba95b86a8e8db493e919d75990b09e30
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-07-13 15:34:39 +0300

    fix: logout button type check

--- src/features/logout/ui/LogoutButton.vue
+++ src/features/logout/ui/LogoutButton.vue
@@ -2,7 +2,7 @@
   <UiButton variant="tertiary-light" @click="account.logout">Выйти</UiButton>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useAccount } from '@/entities/account';
 import { UiButton } from '@/shared/ui';
 
