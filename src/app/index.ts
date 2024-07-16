da864caedf30b511085ba7403c18cac744687f39
Kirill Naumenko <naumenkokirill466@gmail.com>
2024-07-16 23:11:43 +0300

    fix: unexpected error 500 redirect

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,7 @@
-import { queryClient } from '@/shared/lib';
-import { routeNames } from '@/shared/lib';
+import { queryClient, routeNames } from '@/shared/lib';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
-import { useRouter } from 'vue-router';
 import VueToastificationPlugin from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 
@@ -18,7 +16,6 @@ export function initApp() {
   app.use(VueToastificationPlugin);
   app.use(router);
   app.config.errorHandler = (error) => {
-    const router = useRouter();
     console.error(error);
     router.push({ name: routeNames.serverError });
   };
